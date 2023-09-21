3cf6e5b2ee426ed6f154d4aa771b2694eacedea0
alexgabriel8 <alexgabriels8@outlook.com>
2023-09-21 08:37:36 -0300

    chore: project initial state and configs

---
+++ src/global-style.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Hanken Grotesk';
+    }
+
+    body {
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    a {
+        text-decoration: none;
+    }
+    
+    ul, ol {
+        list-style: none;
+    }
+
+`
