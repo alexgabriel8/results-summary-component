bec802e4f1e44b9414a9bbf61c1747a475edfb19
alexgabriel8 <alexgabriels8@outlook.com>
2023-09-26 20:29:25 -0300

    fix(vite.config.ts): add reference types for vitest in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -6,6 +7,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: 'jsdom'
   },
 })
