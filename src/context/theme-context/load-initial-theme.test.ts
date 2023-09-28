2d90a550c3d75eb4d19076eaf2f2838856ebe619
alexgabriel8 <alexgabriels8@outlook.com>
2023-09-28 08:35:10 -0300

    refactor: load-initial-theme.test.ts
    - improve describe hierarchy
    - create matchMediaMock object to avoid code repetition

--- src/context/theme-context/load-initial-theme.test.ts
+++ src/context/theme-context/load-initial-theme.test.ts
@@ -1,54 +1,62 @@
 import { describe, expect, it, beforeEach, vi } from "vitest";
 
 import { loadInitialTheme } from "./load-initial-theme";
-import { saveOnLocalStorage} from "../../scripts/localStorage";
+import { saveOnLocalStorage } from "../../scripts/localStorage";
 
 import { themes } from "../../constants/themes";
 
-describe('Load theme in Local Storage', () => {
-    const key = 'lastUsedTheme'
-
-    beforeEach(() => {
-        localStorage.clear()
+const matchMediaMock = {
+  true: () => (
+    Object.defineProperty(window, 'matchMedia', {
+      value: vi.fn().mockImplementation(() => ({
+        matches: true,
+      }))
     })
-    
+  ),
+
+  false: () => (
+    Object.defineProperty(window, 'matchMedia', {
+      value: vi.fn().mockImplementation(() => ({
+        matches: false,
+      }))
+    })
+  )
+}
+
+describe('Load theme', () => {
+  const key = 'lastUsedTheme'
+
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  describe("Theme saved in local storage", () => {
     it("Should load light theme saved in local storage even if user prefers dark", () => {
-        Object.defineProperty(window, 'matchMedia', {
-            value: vi.fn().mockImplementation(() => ({
-              matches: false,
-            })),
-          })
-          console.log(window.matchMedia('(prefers-color-scheme: light)').matches)
-        saveOnLocalStorage(key, themes.light)
-        expect(loadInitialTheme()).toStrictEqual(themes.light)
+      matchMediaMock.false()
+
+      saveOnLocalStorage(key, themes.light)
+      expect(loadInitialTheme()).toStrictEqual(themes.light)
     })
 
     it("Should load dark theme saved in local storage even if user prefers light", () => {
-        Object.defineProperty(window, 'matchMedia', {
-            value: vi.fn().mockImplementation(() => ({
-              matches: true,
-            })),
-          })
+      matchMediaMock.true()
 
-        saveOnLocalStorage(key, themes.dark)
-        expect(loadInitialTheme()).toStrictEqual(themes.dark)
+      saveOnLocalStorage(key, themes.dark)
+      expect(loadInitialTheme()).toStrictEqual(themes.dark)
     })
+  })
 
+  describe("No theme in local storage", () => {
     it("Should load light theme if user prefers it and none is found in local storage", () => {
-        Object.defineProperty(window, 'matchMedia', {
-            value: vi.fn().mockImplementation(() => ({
-              matches: true,
-            })),
-          })
-        expect(loadInitialTheme()).toStrictEqual(themes.light)
+      matchMediaMock.true()
+
+      expect(loadInitialTheme()).toStrictEqual(themes.light)
     })
 
     it("Should load dark theme if user prefers it and none is found in local storage", () => {
-        Object.defineProperty(window, 'matchMedia', {
-            value: vi.fn().mockImplementation(() => ({
-              matches: false,
-            })),
-          })
-        expect(loadInitialTheme()).toStrictEqual(themes.dark)
+      matchMediaMock.false()
+
+      expect(loadInitialTheme()).toStrictEqual(themes.dark)
     })
+  })
 })
