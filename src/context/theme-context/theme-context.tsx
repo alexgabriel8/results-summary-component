b18d865e7ab0a502563a5098efc19f918382eeee
alexgabriel8 <alexgabriels8@outlook.com>
2023-10-06 06:22:02 -0300

    refactor(theme-context.tsx): change export to inline

--- src/context/theme-context/theme-context.tsx
+++ src/context/theme-context/theme-context.tsx
@@ -5,25 +5,24 @@ import { loadInitialTheme } from "./load-initial-theme";
 
 import { TTheme } from "../../constants/themes";
 
-type TThemeContext = {
+export type TThemeContext = {
     activeTheme: TTheme
     setActiveTheme: React.Dispatch<React.SetStateAction<TTheme>>
 }
 
-const ThemeContext = createContext<TThemeContext | null>(null)
+export const ThemeContext = createContext<TThemeContext | null>(null)
 
-const ThemeProvider = ({ children }: React.PropsWithChildren) => {
+export const ThemeProvider = ({ children }: React.PropsWithChildren) => {
     const [ activeTheme, setActiveTheme ] = useState(loadInitialTheme())
 
     useEffect(() => {
+        
         saveOnLocalStorage('lastUsedTheme', activeTheme)
     }, [activeTheme])
 
     return(
-        <ThemeContext.Provider value={{activeTheme, setActiveTheme}}>
+        <ThemeContext.Provider value={{activeTheme, setActiveTheme}!}>
             {children}
         </ThemeContext.Provider>
     )
 }
-
-export { ThemeContext, TThemeContext, ThemeProvider}
