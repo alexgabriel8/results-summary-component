28bafb941ca24e470f6c8d6f76e09fcfd38c6a3f
alexgabriel8 <alexgabriels8@outlook.com>
2023-10-09 20:37:37 -0300

    func(load-initial-theme): validate local storage theme

--- src/context/theme-context/load-initial-theme.ts
+++ src/context/theme-context/load-initial-theme.ts
@@ -1,11 +1,22 @@
 import { getLocalStorageItem } from "../../scripts/localStorage"
+import { themeLocalStorageKey, TTheme, themes } from "../../constants/themes"
 
-import { themes } from "../../constants/themes"
+function validateTheme(theme: TTheme) {
+    if(
+        JSON.stringify(Object.entries(theme).sort()) === JSON.stringify(Object.entries(themes.light).sort())
+        || JSON.stringify(Object.entries(theme).sort()) === JSON.stringify(Object.entries(themes.dark).sort())
+    ) return theme
+    else return window.matchMedia('(prefers-color-scheme: light)').matches
+            ? themes.light
+            : themes.dark
+}
 
-export function loadInitialTheme() {
-    return getLocalStorageItem('lastUsedTheme') ?? (
-                window.matchMedia('(prefers-color-scheme: light)').matches
+export function loadInitialTheme(): TTheme {
+    const theme = getLocalStorageItem(themeLocalStorageKey) as unknown as TTheme
+    if(!theme) return window.matchMedia('(prefers-color-scheme: light)').matches
                 ? themes.light
                 : themes.dark
-            )
+    else {
+        return validateTheme(theme)
+    }
 }
