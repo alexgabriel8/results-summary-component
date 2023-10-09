18dfccac81c1076ee0fa6940b770598a79b545b9
alexgabriel8 <alexgabriels8@outlook.com>
2023-10-09 20:41:56 -0300

    fix(theme-context): body background-color correctly loaded

--- src/context/theme-context/theme-context.tsx
+++ src/context/theme-context/theme-context.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect, createContext } from "react";
 
+import { themeLocalStorageKey } from "../../constants/themes";
+
 import { saveOnLocalStorage } from "../../scripts/localStorage";
 import { loadInitialTheme } from "./load-initial-theme";
 
 import { TTheme } from "../../constants/themes";
+
+import { themes } from "../../constants/themes";
 
 export type TThemeContext = {
     activeTheme: TTheme
@@ -13,11 +17,17 @@ export type TThemeContext = {
 export const ThemeContext = createContext<TThemeContext | null>(null)
 
 export const ThemeProvider = ({ children }: React.PropsWithChildren) => {
-    const [ activeTheme, setActiveTheme ] = useState(loadInitialTheme())
+    const [ activeTheme, setActiveTheme ] = useState(loadInitialTheme()) as unknown as TTheme
 
     useEffect(() => {
-        
-        saveOnLocalStorage('lastUsedTheme', activeTheme)
+        if(activeTheme.name === 'light') {
+            document.querySelector('body')!.style.backgroundColor = themes.light.bgColor
+        } else if(activeTheme.name === 'dark') {
+            document.querySelector('body')!.style.backgroundColor = themes.dark.bgColor
+        }
+    }, [])
+    useEffect(() => {
+        saveOnLocalStorage(themeLocalStorageKey, activeTheme)
     }, [activeTheme])
 
     return(
